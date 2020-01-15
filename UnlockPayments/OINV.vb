4103e1ec53ee5ad400c12d867476c46a3a21486a
raiden16 <siul-luna@hotmail.com>
2020-01-15 17:30:53 -0600

    Agregar boton

---
+++ UnlockPayments/OINV.vb
@@ -0,0 +1,43 @@
+﻿Public Class OINV
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company     '//OBJETO DE CONEXION
+    Private coForm As SAPbouiCOM.Form           '//FORMA
+    Private Directorio As String
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+        Directorio = oCatchingEvents.csDirectory
+    End Sub
+
+    '//----- AGREGA ELEMENTOS A LA FORMA
+    Public Sub addFormItems(ByVal FormUID As String)
+        Dim loItem As SAPbouiCOM.Item
+        Dim loButton As SAPbouiCOM.Button
+        Dim lsItemRef As String
+
+        Try
+            '//AGREGA BOTON MOVIMIENTOS EN PEDIDOS DE COMPRAS
+            coForm = cSBOApplication.Forms.Item(FormUID)
+            lsItemRef = "2"
+            loItem = coForm.Items.Add("btPay", SAPbouiCOM.BoFormItemTypes.it_BUTTON)
+            loItem.Left = coForm.Items.Item(lsItemRef).Left + coForm.Items.Item(lsItemRef).Width + 5
+            loItem.Top = coForm.Items.Item(lsItemRef).Top
+            loItem.Width = coForm.Items.Item(lsItemRef).Width + 40
+            loItem.Height = coForm.Items.Item(lsItemRef).Height
+            loButton = loItem.Specific
+            loButton.Caption = "Borrar Pago"
+
+        Catch ex As Exception
+            cSBOApplication.MessageBox("DocumentoSBO. agregar elementos a la forma. " & ex.Message)
+        Finally
+            coForm = Nothing
+            loItem = Nothing
+            loButton = Nothing
+        End Try
+    End Sub
+
+End Class
---
+++ UnlockPayments/UnlockPay.vb
@@ -0,0 +1,23 @@
+﻿Public Class UnlockPay
+
+    Private cSBOApplication As SAPbouiCOM.Application '//OBJETO DE APLICACION
+    Private cSBOCompany As SAPbobsCOM.Company     '//OBJETO DE CONEXION
+    Private coForm As SAPbouiCOM.Form           '//FORMA
+    Private csFormUID As String
+    Private stDocNum As String
+    Friend Monto As Double
+
+
+    '//----- METODO DE CREACION DE LA CLASE
+    Public Sub New()
+        MyBase.New()
+        cSBOApplication = oCatchingEvents.SBOApplication
+        cSBOCompany = oCatchingEvents.SBOCompany
+        Me.stDocNum = stDocNum
+    End Sub
+
+    Public Function Valid(ByVal DocNum As String)
+
+    End Function
+
+End Class
