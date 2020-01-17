591c239374cac0de8a50a334102220cafaa44f7c
raiden16 <siul-luna@hotmail.com>
2020-01-17 16:51:12 -0600

    NotaCredito

--- UnlockPayments/UnlockPay.vb
+++ UnlockPayments/UnlockPay.vb
@@ -47,7 +47,7 @@
                 Pago2 = oRecSetH.Fields.Item("U_CSM_IMPORTEPAGADO2").Value
 
                 Payment(CardCode, FDeposito, Pago, FDeposito1, Pago1, FDeposito2, Pago2)
-                ORINA()
+                ORINA(DocNum)
 
             Else
 
@@ -103,7 +103,84 @@
 
     End Function
 
-    Public Function ORINA()
+    Public Function ORINA(ByVal DocNum As String)
+
+        Dim stQueryH, stQueryH2 As String
+        Dim oRecSetH, oRecSetH2 As SAPbobsCOM.Recordset
+        Dim oORINA As SAPbobsCOM.Documents
+        Dim DocEntry, DocCur As String
+        Dim llError As Long
+        Dim lsError As String
+
+        oRecSetH = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+        oRecSetH2 = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset)
+
+        Try
+
+            stQueryH = "Select ""DocEntry"",""CardCode"",""SlpCode"",""Project"",""DocTotal"",""DocCur"" from OINV where ""DocNum""=" & DocNum
+            oRecSetH.DoQuery(stQueryH)
+
+            If oRecSetH.RecordCount > 0 Then
+
+                oORINA = cSBOCompany.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oCreditNotes)
+                oRecSetH.MoveFirst()
+
+                DocEntry = oRecSetH.Fields.Item("DocEntry").Value
+                DocCur = oRecSetH2.Fields.Item("DocCur").Value
+
+                oORINA.Series = "NC"
+                oORINA.CardCode = oRecSetH.Fields.Item("CardCode").Value
+                oORINA.DocDate = Now.Date
+                oORINA.DocDueDate = Now.Date
+                oORINA.DocTotal = oRecSetH.Fields.Item("DocTotal").Value
+                oORINA.SalesPersonCode = oRecSetH.Fields.Item("SlpCode").Value
+                oORINA.DocumentsOwner = 38
+                oORINA.Indicator = oRecSetH.Fields.Item("SlpCode").Value
+                oORINA.UserFields.Fields.Item("U_WhsCodeC").Value = oRecSetH.Fields.Item("Project").Value
+
+                stQueryH2 = "Select ""ObjType"",""LineNum"",""ItemCode"",""Price"",""Quantity"",""TaxCode"",""WhsCode"",""Project"",""DiscPrcnt"" from INV1 where ""DocEntry""=" & DocEntry
+                oRecSetH2.DoQuery(stQueryH2)
+
+                If oRecSetH2.RecordCount > 0 Then
+
+                    oRecSetH2.MoveFirst()
+
+                    For l = 0 To oRecSetH2.RecordCount - 1
+
+                        oORINA.Lines.ItemCode = oRecSetH2.Fields.Item("ItemCode").Value
+                        oORINA.Lines.BaseType = oRecSetH2.Fields.Item("ObjType").Value
+                        oORINA.Lines.BaseLine = oRecSetH2.Fields.Item("LineNum").Value
+                        oORINA.Lines.BaseEntry = DocEntry
+                        oORINA.Lines.Price = oRecSetH2.Fields.Item("Price").Value
+                        oORINA.Lines.Quantity = oRecSetH2.Fields.Item("Quantity").Value
+                        oORINA.Lines.TaxCode = oRecSetH2.Fields.Item("TaxCode").Value
+                        oORINA.Lines.WarehouseCode = oRecSetH2.Fields.Item("WhsCode").Value
+                        oORINA.Lines.ProjectCode = oRecSetH2.Fields.Item("Project").Value
+                        oORINA.Lines.DiscountPercent = oRecSetH2.Fields.Item("DiscPrcnt").Value
+                        oORINA.Lines.Currency = DocCur
+
+                        oORINA.Lines.Add()
+
+                        oRecSetH2.MoveNext()
+
+                    Next
+
+                End If
+
+                If oORINA.Add() <> 0 Then
+
+                    cSBOCompany.GetLastError(llError, lsError)
+                    Err.Raise(-1, 1, lsError)
+
+                End If
+
+            End If
+
+        Catch ex As Exception
+
+            cSBOApplication.MessageBox("Error al Crear Nota de Crédito: " & ex.Message)
+
+        End Try
 
     End Function
 
